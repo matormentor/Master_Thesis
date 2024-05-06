a94af86e1df51008607f704b77303f4671ca568e
Mateo Argudo <mateoarg98@hotmail.com>
2024-05-06 23:59:27 +0200

    start repo

---
+++ make.bat
@@ -0,0 +1,7 @@
+pdflatex --interaction=nonstopmode -halt-on-error -file-line-error ClassicThesis.tex
+bibtex ClassicThesis
+bibtex ClassicThesis
+pdflatex --interaction=nonstopmode -halt-on-error -file-line-error ClassicThesis.tex
+pdflatex --interaction=nonstopmode -halt-on-error -file-line-error ClassicThesis.tex
+pdflatex --interaction=nonstopmode -halt-on-error -file-line-error ClassicThesis.tex
+
